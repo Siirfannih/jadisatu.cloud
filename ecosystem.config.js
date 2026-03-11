e87dd819636ffb36f1e7079203d2dbd94f97cac0
IGedeIrfanKusumaPutra <igedeirfankusumaputra@Mac.mshome.net>
2026-03-11 16:02:09 +0800

    fix: use ecosystem.config.js for pm2 deployment to completely eliminate cluster process crashes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       name: "jadisatu-nextjs",
       cwd: "./nextjs-app",
-      script: "node_modules/.bin/next",
+      script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
       env: {
         NODE_ENV: "production",
